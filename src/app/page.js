326dcf5ff45ab73468f2d328abf98d7418d19d77
jitender-english <155473001+jitender-english@users.noreply.github.com>
2024-01-24 11:54:48 +0530

    Updated
    Updated

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
       <main>
         <div className="content-wrapper">
           <ul>
-            <li><Link href="/lesson-01"><span className="lesson-marker">Lesson 01</span> The Use of 'is, am, are'</Link></li>
-            <li><Link href="/lesson-02"><span className="lesson-marker">Lesson 02</span> The Verb 'to be' - Past Tense</Link></li>
-            <li><Link href="/lesson-03"><span className="lesson-marker">Lesson 03</span> The Use of 'Has, Have, Had'</Link></li>
+            <li><Link href="/lesson-01"><span className="lesson-marker">Lesson 01</span> The Use of &#39;is, am, are&#39;</Link></li>
+            <li><Link href="/lesson-02"><span className="lesson-marker">Lesson 02</span> The Verb &#39;to be&#39; - Past Tense</Link></li>
+            <li><Link href="/lesson-03"><span className="lesson-marker">Lesson 03</span> The Use of &#39;Has, Have, Had&#39;</Link></li>
             <li><Link href="/lesson-04"><span className="lesson-marker">Lesson 04</span> Present Indefinite Tense</Link></li>
             <li><Link href="/lesson-05"><span className="lesson-marker">Lesson 05</span> Present Continuous Tense</Link></li>
             <li><Link href="/lesson-06"><span className="lesson-marker">Lesson 06</span> Present Perfect Tense</Link></li>
@@ -31,11 +31,15 @@ export default function Home() {
           <br /><hr /><br />
           <ul>
             <li>Reacher - 
-              <Link href="/english/season/reacher/s02e01"> <span className='color-dark-secondary'>S01E01 </span></Link>, 
-              <Link href="/english/season/reacher/s02e02"><span className='color-dark-secondary'>S01E02 </span></Link> 
-              <Link href="/english/season/reacher/s02e03"><span className='color-dark-secondary'>S01E03 </span></Link> 
-              <Link href="/english/season/reacher/s02e04"><span className='color-dark-secondary'>S01E04 </span></Link></li>
-            <li>Criminal Record - <Link href="/english/season/criminal-record/s01e01"> <span className='color-dark-secondary'>S01E01 </span></Link><Link href="/english/season/criminal-record/s01e02"><span className='color-dark-secondary'>S01E02 </span></Link></li>
+              <Link href="/english/season/reacher/s02e01"> <span className="color-dark-secondary">S01E01 </span></Link>, 
+              <Link href="/english/season/reacher/s02e02"><span className="color-dark-secondary">S01E02 </span></Link> 
+              <Link href="/english/season/reacher/s02e03"><span className="color-dark-secondary">S01E03 </span></Link> 
+              <Link href="/english/season/reacher/s02e04"><span className="color-dark-secondary">S01E04 </span></Link>
+            </li>
+            <li>Criminal Record - 
+              <Link href="/english/season/criminal-record/s01e01"> <span className="color-dark-secondary">S01E01 </span></Link>
+              <Link href="/english/season/criminal-record/s01e02"><span className="color-dark-secondary">S01E02 </span></Link>
+            </li>
           </ul>
         </div>
       </main>
