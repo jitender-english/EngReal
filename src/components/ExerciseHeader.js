14497b0cbcbd6349bf7fcc00f4326280b09f2b53
jitender-english <155473001+jitender-english@users.noreply.github.com>
2024-02-11 08:42:16 +0530

    Updated
    Updated

--- src/components/ExerciseHeader.js
+++
@@ -1,8 +0,0 @@
-export default function ExerciseHeader() {
-    return (
-        <header className="ffSeg tC mb32">
-            <h2 className="mb0">Exercise</h2>
-            <p>Translate the following into English:</p>
-        </header>
-    );
-}
